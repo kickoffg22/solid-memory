24efc75223d83c5fa6e5f47cf0a02a23916bfc29
FY-Wang <51990231+FY-Wang@users.noreply.github.com>
2019-06-27 23:37:09 +0200

    Create rgb_to_gray.m
    Convert 3 channel RGB images to grey scale.

---
+++ rgb_to_gray.m
@@ -0,0 +1,15 @@
+function gray_image = rgb_to_gray(input_image)
+    % Diese Funktion soll ein RGB-Bild in ein Graustufenbild umwandeln. Falls
+    % das Bild bereits in Graustufen vorliegt, soll es direkt zurueckgegeben werden.
+    imgsize = size(input_image);
+    if numel(imgsize) < 3
+        gray_image = input_image;
+    else
+        image2 = double(input_image);
+        R=image2(:,:,1);%R Channel
+        G=image2(:,:,2);%G Channel
+        B=image2(:,:,3);%B Channel   
+        gray_image = 0.299*R+0.587*G+0.114*B;
+        gray_image = uint8(gray_image);
+    end
+end
