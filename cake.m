dbc0d6e2d766e046c375f8acd5335d23b928cd57
FY-Wang <51990231+FY-Wang@users.noreply.github.com>
2019-07-06 16:49:00 +0200

    Adptive window and occulusion detection

--- cake.m
+++
@@ -1,8 +0,0 @@
-function Cake = cake(min_dist)
-    % Die Funktion cake erstellt eine "Kuchenmatrix", die eine kreisfoermige
-    % Anordnung von Nullen beinhaltet und den Rest der Matrix mit Einsen
-    % auffuellt. Damit koennen, ausgehend vom staerksten Merkmal, andere Punkte
-    % unterdrueckt werden, die den Mindestabstand hierzu nicht einhalten. 
-    [X_cake,Y_cake] = meshgrid(-min_dist:min_dist,-min_dist:min_dist);
-    Cake = X_cake.^2+Y_cake.^2>min_dist^2;    
-end
