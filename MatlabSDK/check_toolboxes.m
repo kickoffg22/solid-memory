d597088228a0ff9e24d2fb2d94de4a905cec1558
congw112358 <50798162+congw112358@users.noreply.github.com>
2019-07-14 21:41:00 +0200

    check_toolboxes_21.40_0714

--- MatlabSDK/check_toolboxes.m
+++ MatlabSDK/check_toolboxes.m
@@ -1,10 +1,9 @@
-function toolbox_in_use = check_toolboxes(func_name)
-[~, pList] = matlab.codetools.requiredFilesAndProducts(func_name);
-if (size({pList, Name}', 1)>1)
-    toolbox_in_use = ture;
-    displ('Toolbox wird verwendet')
-else
-    toolbox_in_use = false;
-    displ('Keine Toolboxen werden verwendet')
-end
-end
+
+    function toolboxes_in_use = check_toolboxes(f_name)
+    [~,pList] = matlab.codetools.requiredFilesAndProducts(f_name);
+    if (size({pList.Name}', 1)>1)
+        toolboxes_in_use = ture;
+    else
+        toolboxes_in_use = false;
+    end
+    end
