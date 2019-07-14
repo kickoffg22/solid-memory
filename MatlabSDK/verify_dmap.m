5a7c726bec993a6fae02b74e3ea8a7127c87bddb
Fengyi Wang <51990231+FY-Wang@users.noreply.github.com>
2019-07-14 21:04:10 +0200

    reference

--- MatlabSDK/verify_dmap.m
+++ MatlabSDK/verify_dmap.m
@@ -1,8 +1,14 @@
-function p = verify_dmap(D, G)
+function p = verify_dmap(D, G,varargin)
 if isuint8(D)&&isuint8(G)==0
     error('The input matrix should be of datatype uint8');
 end
-G = uint8(G*255/max(G(:)));%Nomolize the input matrix to interval[0,255];
+p = inputParser;%Input parser for  
+default_calib  = 2;
+validationFcn_calib = @(x) isnumeric(x) && isscalar(x) &&isinteger(x);
+p.addParameter('calib',default_calib,validationFcn_calib);
+calib = p.Results.calib;
+
+G = uint8(G*calib*255/max(G(:)));%Nomolize the input matrix to interval[0,255];
 D = uint8(D*255/max(G(:)));
 p = calcPSNR(D,G);
 end
