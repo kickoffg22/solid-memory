287e714743b73276d02a09aa09e7dda4cfc5932d
VinceZhaowenjian <52327282+VinceZhaowenjian@users.noreply.github.com>
2019-07-16 21:24:31 +0200

    Update disparity_map.m

--- MatlabSDK/disparity_map.m
+++ MatlabSDK/disparity_map.m
@@ -15,33 +15,6 @@ end
     D = uint8(Refined_DisparityMap/calib);% The map is divided by a calibration index to prevent overflow.
 %% T,R calculation
 
-    % Diese Funktion berechnet die moeglichen Werte fuer T und R
-    % aus der Essentiellen Matrix
-    %% Bilder laden
-    Image1 = I{1};
-    IGray1 = rgb_to_gray(Image1);
-    Image2 = I{2};
-    IGray2 = rgb_to_gray(Image2);
-
-    %% Harris-Merkmale berechnen
-    Merkmale1 = harris_detektor(IGray1,'segment_length',15,'k',0.05,'min_dist',20,'N',5,'do_plot',false);
-    Merkmale2 = harris_detektor(IGray2,'segment_length',15,'k',0.05,'min_dist',20,'N',5,'do_plot',false);
-    %% Korrespondenzschaetzung
-    Korrespondenzen = punkt_korrespondenzen(IGray1,IGray2,Merkmale1,Merkmale2,'window_length',25,'min_corr',0.95,'do_plot',false);
-    %% Berechne die Essentielle Matrix
-    E = achtpunktalgorithmus(Korrespondenzen);
-    %%
-    [U,S,V] = svd(E);
-    U = U * [1 0 0; 0 1 0; 0 0 -1];
-    V = V * [1 0 0; 0 1 0; 0 0 -1]; %don't know why should i do this?
-    R_zp = [0 -1  0;
-           1  0  0;
-           0  0  1];
-
-    R = U*R_zp'*V';
-    T_hat = U*R_zp*S*U';
-    T = [T_hat(3,2);
-          T_hat(1,3);
-          T_hat(2,1)];
+    [T,R]=TR()
 
 end
